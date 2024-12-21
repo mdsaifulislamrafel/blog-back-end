dfd734cccd867d9af95ff10a63c999e8dc2971fd
Md Saiful Islam Rafel <mdsaifulisalmrafel.com>
2024-12-21 19:10:12 +0600

    user create data return parten change

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -4,20 +4,23 @@ import { userServices } from "./user.service";
 import httpStatus from "http-status";
 
 const createRegisterUser = catchAsync(async (req, res) => {
-
   const result = await userServices.registerUserIntoDB(req.body);
-  
+
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: "User registered successfully",
-    data: result,
+    data: {
+      _id: result._id,
+      name: result.name,
+      email: result.email,
+    },
   });
 });
 
 const loginUser = catchAsync(async (req, res) => {
   const result = await userServices.loginUserIntoDB(req.body);
-  
+
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
